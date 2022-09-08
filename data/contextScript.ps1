c7cfb46a94dadd38229203a92f5271fa24249d5a
The Man <ThisIsntTheWay@users.noreply.github.com>
2022-09-08 22:54:04 +0200

    Download $intuneWinTool if missing, add Err to console

--- data/contextScript.ps1
+++ data/contextScript.ps1
@@ -23,7 +23,15 @@ Add-Type -AssemblyName PresentationFramework
 try {
 	# Check if required files exist
 	if (!(Test-Path $intuneWinTool)) {
-		throw "IntuneWinAppUtil was not found at '$intuneWinTool'."
+		# Attempt download
+		Write-Warning "IntuneWinAppUtil not found at '$intuneWinTool', attemtping download..."
+		try {
+			Invoke-RestMethod "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe" `
+				-OutFile $intuneWinTool
+			Write-Host "> Download OK, proceeding..." -f green
+		} catch {
+			throw "Download failure: $_"
+		}
 	}
 	if ($inventory.name -notcontains $installationFile) {
 		throw "Did not find installation file: '$installationFile'."
@@ -55,6 +63,7 @@ try {
 	$temp = gci $path | ? Name -like *.intunewin | Select -first 1	
 	[System.Windows.MessageBox]::Show("Your file is ready at: $($temp.FullName)", "Done", 'Ok', 'Information') | out-null
 } catch {
+	Write-Host "Error: $_" -f red
 	[System.Windows.MessageBox]::Show("Could not create intune package: $($_)", "Error - $path", 'Ok', 'Error') | out-null
 } finally {
 	# Cleanup
